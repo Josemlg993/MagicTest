0eb82c72c5ae9695d0b76f0231b3aecad30bb212
Josemlg993 <94807261+Josemlg993@users.noreply.github.com>
2024-05-20 07:58:19 +0200

    Update loginCheck.js

--- js/loginCheck.js
+++ js/loginCheck.js
@@ -1,18 +1,23 @@
-const loggedOutLinks = document.querySelectorAll (".logged-out")
-const loggedInLinks = document.querySelectorAll (".logged-in")
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+    const loggedInLinks = document.querySelectorAll(".logged-in");
 
-console.log (loggedOutLinks)
-console.log (loggedInLinks)
+    console.log(loggedOutLinks);
+    console.log(loggedInLinks);
 
-export const loginCheck = user => {
-    if (user) {
-        // El usuario está autenticado
-        loggedOutLinks.forEach(link => link.style.display = "none")
-        loggedInLinks.forEach(link => link.style.display = "block")
+    export const loginCheck = user => {
+        if (user) {
+            // El usuario está autenticado
+            loggedOutLinks.forEach(link => link.style.display = "none");
+            loggedInLinks.forEach(link => link.style.display = "block");
+        } else {
+            // El usuario no está autenticado
+            loggedOutLinks.forEach(link => link.style.display = "block");
+            loggedInLinks.forEach(link => link.style.display = "none");
 
-    } else {
-        // El usuario no está autenticado
-        loggedOutLinks.forEach(link => link.style.display = "block")
-        loggedInLinks.forEach(link => link.style.display = "none")
+            // Abre el modal de inicio de sesión después de 3 segundos
+            setTimeout(() => {
+                let loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
+                loginModal.show();
+            }, 3000);
+        }
     }
-}
